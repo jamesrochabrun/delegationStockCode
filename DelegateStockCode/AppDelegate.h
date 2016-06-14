54fb37e3ac8c3dc85ffb718aefec9bd03b85cbc6
james <jamesrochabrun@gmail.com>
2016-06-14 11:53:28 -0400

    Initial Commit

---
+++ DelegateStockCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DelegateStockCode
+//
+//  Created by James Rochabrun on 14-06-16.
+//  Copyright © 2016 James Rochabrun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DelegateStockCode/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DelegateStockCode
+//
+//  Created by James Rochabrun on 14-06-16.
+//  Copyright © 2016 James Rochabrun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
